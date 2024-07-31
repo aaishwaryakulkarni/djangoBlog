26040cb78da24339b9b957ad49aa82f9015c12e6
aaishwaryakulkarni <aishwarya.kulkarni09@gmail.com>
2024-07-31 17:23:17 +0100

    login logout functionality

--- users/forms.py
+++ users/forms.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegister(UserCreationForm):
-    email = forms.EmailField( )
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
